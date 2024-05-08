0759b4b16cfaf527c0e08e50fcd4b609dd07c202
MedanMv <ivanmarchenko8002@gmail.com>
2024-05-08 21:10:24 +0300

    Done the checklist

---
+++ blocks/cover/__overlay/cover__overlay.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cover__overlay = document.querySelector('.cover__overlay');
+    
+    cover__overlay.addEventListener('click', function() {
+        window.location.href = 'https://www.nps.gov/nr/travel/route66/maps66.html';
+    });
+});
